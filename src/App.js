2060d5c3fae850b34cf1e4f867aa091a13d454a2
chesterdany <chesterdany22@yahoo.com>
2021-01-30 13:48:30 +0200

    api folder to fetch weather data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
 
+import { useState } from "react";
+
+import fetchWeather from "./api/fetchWeather";
+
 function App() {
-  return <h1>App</h1>;
+  const [query, setQuery] = useState("");
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+
+      console.log(data);
+    }
+  };
+
+  return (
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+    </div>
+  );
 }
 
 export default App;
